b9e8aad308d4694397190999359a2e46a2ab45eb
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2023-11-14 10:12:01 +0900

    [level 1] Title: 폰켓몬, Time: 4.48 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -4,12 +4,12 @@ class Solution {
     public int solution(int[] nums) {
         int answer = 0;
         
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         
         for(int num : nums) {
             set.add(num);
         }
         
-        return set.size() > nums.length/2? nums.length/2 : set.size();
+        return Math.min(set.size(), nums.length/2);
     }
 }
