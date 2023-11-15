b5826aead2cdcf2b52d302704d9f445f66dd0ee5
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2023-11-15 23:51:29 +0900

    [level 3] Title: 베스트앨범, Time: 3.37 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    
+    static class Music {
+        private int index;
+        private String genre;
+        private int play;
+        
+        public Music(int index, String genre, int play) {
+            this.index = index;
+            this.genre = genre;
+            this.play = play;
+        }
+        
+        public int getIndex() {
+            return this.index;
+        }
+    }
+    
+    public int[] solution(String[] genres, int[] plays) {
+        // 1. 재생수 높은 장르 <장르, 재생수>
+        Map<String, Integer> hashmap = new HashMap<>();
+        for(int i=0; i<genres.length; i++) {
+            hashmap.put(genres[i], hashmap.getOrDefault(genres[i], 0) + plays[i]);
+        }
+        
+        // 재생수 순으로 정렬
+        List<String> ordered_genre = new ArrayList<>(hashmap.keySet());
+        ordered_genre.sort((o1, o2) -> hashmap.get(o2).compareTo(hashmap.get(o1)));
+        
+        // 2. 장르 내에서 가장 많이 재생된 노래
+        // 모든 정보 -> Music 객체로
+        List<Music> musicList = new ArrayList<>();
+        
+        for(int i=0; i<genres.length; i++) {
+            musicList.add(new Music(i, genres[i], plays[i]));
+        }
+        Collections.sort(musicList, (o1, o2) -> o2.play - o1.play);
+        
+        // 장르별 2곡씩 선정
+        List<Music> bestAlbum = new ArrayList<>();
+        
+//         for(String genre : ordered_genre) {
+//             // 2곡씩 담을 list
+//             List<Music> list = new ArrayList<>();
+//             // for(int i=0; i<genres.length; i++) {
+//             for(int i=0; i<musicList.size(); i++) {
+//                 if(genre.equals(musicList.get(i).genre) && list.size()<2) {
+//                     list.add(musicList.get(i));
+//                 }
+//             }
+            
+//             // list 속 Music(2개) 고유번호 기준 정렬
+//             Collections.sort(list,(o1, o2) -> o2.index - o1.index);
+//             for(int i=0; i<list.size(); i++) {
+//                 bestAlbum.add(list.get(i));
+//             }
+//             // bestAlbum.addAll(list);
+//         }
+        
+        for (String genre : ordered_genre) {
+    // 2곡씩 담을 list
+    List<Music> list = new ArrayList<>();
+    for (int i = 0; i < musicList.size(); i++) {
+        if (genre.equals(musicList.get(i).genre)) {
+            list.add(musicList.get(i));
+        }
+    }
+
+    // list 속 Music(2개) 고유번호 기준 정렬
+    Collections.sort(list, (o1, o2) -> o2.play - o1.play);
+    
+    // Add the top two songs for each genre to the bestAlbum
+    bestAlbum.add(list.get(0));
+    if (list.size() > 1) {
+        bestAlbum.add(list.get(1));
+    }
+}
+        
+        // 고유번호(index)만 추출
+        int[] answer = bestAlbum.stream()
+            .mapToInt(Music::getIndex)
+            .toArray();
+        
+        return answer;
+    }
+}
