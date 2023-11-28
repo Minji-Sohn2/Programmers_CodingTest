cce55a529fdf689532c8b835393d851cd1a0f2ba
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2023-11-29 07:26:20 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int size = brown + yellow;
+        
+        for(int i=3; i<=size; i++) {
+            if(size%i == 0) {
+                int row = Math.min(i, size/i);
+                int col = Math.max(i, size/i);
+                
+                // 이 경우 갈색 개수
+                int bNum = 2 * (col + row -2);
+                if(bNum == brown) {
+                    answer[0] = col;
+                    answer[1] = row;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
