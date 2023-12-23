33fd6244c3447e627148f9df8684c6017188e231
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2023-12-23 09:41:33 +0900

    [level 2] Title: 문자열 압축, Time: 8.81 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,38 +1,34 @@
 class Solution {
             
     public int solution(String s) {
-        
         int answer = s.length();
         
-        if(s.length()==1) return 1;
-                
         for(int i=1; i<=s.length()/2; i++) {
-            String unit = s.substring(0,i); // 단위
-            int cnt = 1;     // 단위 반복 횟수
-            StringBuilder sb = new StringBuilder();
-            
-            for(int j=i; j<=s.length(); j+=i) {
-                // 다음 i 길이의 문자열
-                String nextUnit = s.substring(j, (j + i > s.length()) ? s.length() : j + i);
-                    
-                if(unit.equals(nextUnit)) {
-                    cnt++;
-                } else {
-                    // 반복이 있는 경우 반복 횟수 문자열에 추가
-                    if(cnt>1) {
-                        sb.append(cnt);
-                    }
-                    sb.append(unit);
-                    unit = nextUnit;
-                    cnt = 1;
-                }
-            }
-            
-            sb.append(unit);
-            
-            answer = Math.min(answer, sb.length());
+            int result = zipString(s, i, 1).length();
+            answer = Math.min(answer, result);
         }
         
         return answer;
+    }
+    
+    private String zipString(String s, int i, int cnt) {
+        // s 에 들어올 string은 계속 짧아지기 때문에
+        if(s.length() < i) {
+           return s; 
+        } 
+        
+        StringBuilder sb = new StringBuilder();
+        String pre = s.substring(0,i);
+        String post = s.substring(i, s.length());
+        
+        // post의 시작과 pre 가 다르면
+        if(!post.startsWith(pre)) {
+            sb.append((cnt == 1) ? pre : cnt + pre);
+            sb.append(zipString(post, i, 1));
+        } else {
+            sb.append(zipString(post, i, ++cnt));
+        }
+        
+        return sb.toString();
     }
 }
