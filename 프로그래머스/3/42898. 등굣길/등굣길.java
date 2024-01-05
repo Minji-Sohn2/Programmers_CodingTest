14bc0b211084daabfe03a70b94cefba32c5689b4
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-01-05 15:07:24 +0900

    [level 3] Title: 등굣길, Time: 0.66 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,32 +1,33 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n + 1][m + 1];
+        int[][] map = new int[n + 1][m + 1];
         int div = 1000000007;
-        dp[1][1] = 1;
+        map[1][1] = 1;
 
         // 웅덩이 초기화
         for (int[] puddle : puddles) {
             int x = puddle[1];
             int y = puddle[0];
-            dp[x][y] = -1;
+            map[x][y] = -1;
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (dp[i][j] == -1) {
-                    dp[i][j] = 0;
+                // 물 웅덩이를 만나면
+                if (map[i][j] == -1) {
+                    map[i][j] = 0;
                     continue;
                 }
 
-                if (i > 1 || j > 1) {
-                    int left = (j > 1) ? dp[i][j - 1] : 0;
-                    int up = (i > 1) ? dp[i - 1][j] : 0;
-
-                    dp[i][j] = (left + up) % div;
+                if (i!=1) {
+                    map[i][j] += map[i-1][j]%div;
+                }
+                if (j!=1) {
+                    map[i][j] += map[i][j-1]%div;
                 }
             }
         }
 
-        return dp[n][m];
+        return map[n][m]%div;
     }
 }
