e942219f835fc1d796e354c6a4ad9fd13efe9e14
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-01-05 12:47:36 +0900

    [level 2] Title: 전화번호 목록, Time: 454.64 ms, Memory: 209 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,23 +4,30 @@ class Solution {
     
     public boolean solution(String[] phone_book) {
         
+        // 전화번호와 해당 인덱스를 저장하기 위한 HashMap 생성
         Map<String, Integer> map = new HashMap<>();
         
+        // HashMap에 각 전화번호와 인덱스를 저장
         for(int i=0; i<phone_book.length; i++) {
             map.put(phone_book[i], i);
         }
         
+        // 각 전화번호에 대해 반복
         for (String key : map.keySet()) {
             String prefix = "";
+            
+            // 전화번호의 각 숫자에 대해 반복
             for (char num : key.toCharArray()) {
                 prefix += num;
 
+                // 현재까지의 접두사가 HashMap에 존재하면서 현재 전화번호와 다른 경우, false 반환
                 if (map.containsKey(prefix) && !prefix.equals(key)) {
                     return false;
                 }
             }
         }
         
+        // 모든 전화번호에 대한 검사를 완료하면 true 반환
         return true;
     }
 }
