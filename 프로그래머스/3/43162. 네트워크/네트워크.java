c46b30a3c05c4ede590c57549ec9062838ed5603
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-01-24 13:11:25 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,41 +1,30 @@
 import java.util.*;
 
 class Solution {
-    
     private boolean[] visited;
-    private Queue<Integer> queue = new LinkedList<>();
     
     public int solution(int n, int[][] computers) {
-        int answer=0;
+        int cnt = 0;
         visited = new boolean[n];
-        
-        for(int i=0; i<n; i++) {
-            // 아직 방문하지 않은 컴퓨터라면
+
+        for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                bfs(i, computers, n);
-                answer++;
+                dfs(computers, i);
+                // dfs 종료 : 네트워크 완성
+                cnt++;
             }
         }
-        
-        return answer;
+        return cnt;
     }
-    
-    // queue에 방문할 노드를 넣어두고 하나씩 뽑아가며 연결된 노드를 다시 넣음
-    private void bfs(int start, int[][] computers, int n) {
-        queue.offer(start);
-        visited[start]=true;
+
+    private void dfs(int[][] computers, int v) {
+        // 현재 컴퓨터 방문
+        visited[v] = true;
         
-        while(!queue.isEmpty()) {
-            // 현재 방문 노드
-            int current = queue.poll();
-            
-            // 현재 노드와 연결된 노드들 확인
-            for(int connect=0; connect<n; connect++) {
-                // 아직 방문하지 않았고 현재 노드와 연결된 경우
-                if(!visited[connect] && computers[current][connect]==1) {
-                    visited[connect]=true;
-                    queue.offer(connect);
-                }
+        // 현재 컴퓨터와 연결된 모든 컴퓨터 dfs 재귀
+        for(int i = 0; i < computers.length; i++) {
+            if(computers[v][i] == 1 && !visited[i]) {
+                dfs(computers, i);
             }
         }
     }
