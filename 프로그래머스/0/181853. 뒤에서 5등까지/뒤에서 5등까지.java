0767c885e48c4286609f1a0b98ee5a0956e358ce
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-01-30 11:00:37 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 2.46 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        int[] answer = Arrays.copyOf(num_list, 5);
-        
-        return answer;
+
+        return Arrays.stream(num_list)
+            .sorted()
+            .limit(5)
+            .toArray();
     }
 }
