b9a3bb0e4b7e0c4d0af6c120a540231061adbac5
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-02-08 11:48:49 +0900

    [level 0] Title: 문자열 밀기, Time: 1.77 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -1,19 +1,7 @@
 class Solution {
     public int solution(String A, String B) {
-        int answer = 0;
-  
-        for(int i=0; i<A.length(); i++) {            
-            String newA = rotateString(A, i);
-            if(newA.equals(B)) {
-                return answer;
-            }
-            answer++;
-        }
-        
-        return -1;
+
+        return (B+B).indexOf(A);
     }
-    
-    private String rotateString(String str, int count) {
-        return str.substring(str.length()-count) + str.substring(0, str.length()-count);
-    }
+
 }
