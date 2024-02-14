3678298dc2f3a3c8e9ae2d0e23c1ca7e309b78b7
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-02-14 11:11:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 20.08 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,12 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        StringBuilder half = new StringBuilder();
-        for(int i=1; i<food.length; i++) {
-            for(int j=0; j<food[i]/2; j++) {
-                half.append(i);
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        
-        return half.toString() + "0" + half.reverse().toString();
+
+        return answer;
     }
 }
