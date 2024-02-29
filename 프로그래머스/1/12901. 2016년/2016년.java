d993527ec0e18327bbdfa9c6f8f442933c638a46
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-02-29 23:20:11 +0900

    [level 1] Title: 2016년, Time: 1.38 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,13 +1,7 @@
+import java.time.*;
+
 class Solution {
     public String solution(int a, int b) {
-        int[] months = new int[]{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        String[] week = new String[]{"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-        
-        int monthSum = b;
-        for(int i=0; i<a-1; i++) {
-            monthSum += months[i];
-        }
-        
-        return week[monthSum%7];
+        return LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0,3);
     }
 }
