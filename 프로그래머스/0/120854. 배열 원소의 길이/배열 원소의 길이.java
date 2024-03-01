5f1fc3aedcc49b5b3432448e7f9379d7261c9de6
Minji-Sohn2 <130354169+Minji-Sohn2@users.noreply.github.com>
2024-03-01 11:08:11 +0900

    [level 0] Title: 배열 원소의 길이, Time: 2.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        
+        return Arrays.stream(strlist)
+                    .mapToInt(x -> x.length())
+                    .toArray();
+    }
+}
